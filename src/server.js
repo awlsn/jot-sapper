8aebb67fb6219fdd22a4fc1cbe337095f4cea8fc
Adam Wilson <adam@wlsn.org>
2020-09-29 14:41:22 -0400

    Testing Polka

--- src/server.js
+++ src/server.js
@@ -1,17 +1,23 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
+import sirv from "sirv";
+import polka from "polka";
+import compression from "compression";
+import * as sapper from "@sapper/server";
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const dev = NODE_ENV === "development";
+
+function one(req, res, next) {
+  req.hello = "world";
+  next();
+}
 
 polka() // You can also use Express
   .use(
     compression({ threshold: 0 }),
-    sirv('static', { dev }),
-    sapper.middleware()
+    sirv("static", { dev }),
+    sapper.middleware(),
+    one
   )
-  .listen(PORT, err => {
-    if (err) console.log('error', err);
+  .listen(PORT, (err) => {
+    if (err) console.log("error", err);
   });
